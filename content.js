e1f1fbb308e42ee844e5ef61ae069df7d382de02
id367800 <alekseev-timof@mail.ru>
2026-01-30 18:39:09 +0300

    Fixed some bugs

--- content.js
+++ content.js
@@ -5,20 +5,20 @@ function combineStandards() {
   standard_1_4_2()
   standard_1_4_4()
   standard_2_1_1()
-  standard_2_4_7()
+  //standard_2_4_7()
   standard_3_1_1()
   standard_4_1_2()
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("load", () => {
   combineStandards();
 });
 
-const observer = new MutationObserver(() => {
-  combineStandards();
-});
+// const observer = new MutationObserver(() => {
+//   combineStandards();
+// });
 
-observer.observe(document.documentElement, {
-  childList: true,
-  subtree: true
-});
+// observer.observe(document.documentElement, {
+//   childList: true,
+//   subtree: true
+// });
